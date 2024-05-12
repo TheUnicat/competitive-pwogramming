40fb4cb94febc7347e7585b13c828641c4a8c503
Matthew H <matthew4680@icloud.com>
2024-05-12 02:14:22 -0700

    usaco guide teleportation

--- usaco/usaco_guide/bronze/teleportation.cpp
+++ usaco/usaco_guide/bronze/teleportation.cpp
@@ -2,8 +2,52 @@
 // Created by TheUnicat on 2024-05-11.
 //
 #include <iostream>
+#include <string>
+#include <sstream>
+#include <vector>
+#include <cstdlib>
+#include <fstream>
 
 int main() {
-    std::cout << "Hello, world!" << std::endl; // Print "Hello, world!" to the console
-    return 0; // Exit status of the program
+    std::ifstream infile("teleport.in");
+    std::string input;
+    std::getline(infile, input);
+    std::istringstream iss(input);
+    std::vector<int> parameters;
+
+    int number; // temporary storage
+
+    while (iss >> number) {
+        parameters.push_back(number);
+    }
+
+    int x_coord = parameters[0];
+    int y_coord = parameters[1];
+
+    int teleportation_x = parameters[2];
+    int teleportation_y = parameters[3];
+
+    if (teleportation_y < teleportation_x) {
+        int temp_storage = teleportation_x;
+        teleportation_x = teleportation_y;
+        teleportation_y = temp_storage;
+    }
+
+    if (y_coord < x_coord) {
+        int temp_storage = x_coord;
+        x_coord = y_coord;
+        y_coord = temp_storage;
+    }
+
+    int raw_distance = y_coord - x_coord;
+
+    int teledistance = abs(teleportation_x - x_coord) + abs(teleportation_y - y_coord);
+
+    int min_distance = raw_distance > teledistance ? teledistance : raw_distance;
+
+    std::ofstream outfile("teleport.out");
+    outfile << min_distance << std::endl;
+    outfile.close();
+
+    return 0;
 }
