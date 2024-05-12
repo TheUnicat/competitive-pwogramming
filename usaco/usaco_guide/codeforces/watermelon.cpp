a992bc8b85310cbbfc3a602985c206959a41080e
Matthew H <matthew4680@icloud.com>
2024-05-12 12:25:03 -0700

    usaco guide watermelon cf

---
+++ usaco/usaco_guide/codeforces/watermelon.cpp
@@ -0,0 +1,18 @@
+//
+// Created by Hong Yang on 2024-05-12.
+//
+
+#include <iostream>
+#include <string>
+
+int main() {
+    int number;
+    std::cin >> number;
+
+    if ((number - 2) % 2 == 0 && number > 2) {
+        std::cout << "YES" << std::endl;
+    } else {
+        std::cout << "NO" << std::endl;
+    }
+    return 0;
+}
