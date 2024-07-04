e3cd5b01e8719954b7cc069bfcbc9d4c6b64966c
TheUnicat <matthew4680@icloud.com>
2024-07-03 19:34:50 -0700

    update forest queries

--- usaco/usaco_guide/cses/problemset/easy/forest_queries.cpp
+++ usaco/usaco_guide/cses/problemset/easy/forest_queries.cpp
@@ -3,35 +3,27 @@
 //
 
 #include <iostream>
-#include <string>
-#include <vector>
 using namespace std;
 
 int main() {
     int n, q;
     cin >> n >> q;
-    vector<vector<int> > forest_prefix_sums;
-    vector<vector<bool> > forest;
+    int forest_prefix_sums[n][n];
     for (int i = 0; i < n; i++) {
-        vector<int> row_prefix_sums(n);
-        vector<bool> forest_row(n);
         int row_sum = 0;
         for (int j = 0; j < n; j++) {
             int num;
             char in;
             cin >> in;
             num = in == '*' ? 1 : 0;
-            forest_row[j] = in == '*' ? true : false;
             row_sum += num;
             if (i > 0) {
-                row_prefix_sums[j] = row_sum + forest_prefix_sums[i - 1][j];
+                forest_prefix_sums[i][j] = row_sum + forest_prefix_sums[i - 1][j];
             }
             else {
-                row_prefix_sums[j] = row_sum;
+                forest_prefix_sums[i][j] = row_sum;
             }
         }
-        forest.push_back(forest_row);
-        forest_prefix_sums.push_back(row_prefix_sums);
     }
 
     for (int i = 0; i < q; i++) {
