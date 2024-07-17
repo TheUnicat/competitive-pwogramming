eda1717766999ae52e1bd70ab7b3ed2f20bf9dbf
TheUnicat <matthew4680@icloud.com>
2024-07-17 14:30:07 -0700

    usaco good subarrays

--- usaco/usaco_guide/codeforces/good_subarrays.cpp
+++ usaco/usaco_guide/codeforces/good_subarrays.cpp
@@ -1,3 +1,40 @@
 //
-// Created by Hong Yang on 2024-07-14.
+// Created by TheUnicat on 2024-07-14.
 //
+
+#include <iostream>
+#include <vector>
+#include <set>
+#include <map>
+#include <string>
+using namespace std;
+using ll = long long;
+
+int n;
+
+int main() {
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        ll good_subarrays = 0;
+        int length;
+        map<int, ll> values;
+        set<int> keys;
+        cin >> length;
+        string test;
+        cin >> test;
+        test = '0' + test;
+        int sum = 0;
+        for (int j = 0; j <= length; j++) {
+            sum += test[j] - '0';
+            values[sum - j]++;
+            keys.insert(sum - j);
+        }
+
+        for (int key : keys) {
+            ll permutations = values[key];
+            good_subarrays += (permutations * (permutations - 1)) / 2;
+        }
+
+        cout << good_subarrays << endl;
+    }
+}
--- usaco/usaco_guide/codeforces/greg_and_array.cpp
+++ usaco/usaco_guide/codeforces/greg_and_array.cpp
@@ -1,3 +1,57 @@
 //
-// Created by Hong Yang on 2024-07-11.
+// Created by TheUnicat on 2024-07-11.
 //
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<long long> og_array;
+
+int n, m, k;
+
+
+int main() {
+    cin >> n >> m >> k;
+    vector<long long> array_prefix_sum(n, 0);
+    vector<int> operations_prefix_sum(m, 0);
+    vector<vector<int> > operations(m);
+    vector<pair<int, int> > queries(k);
+    for (int i = 0; i < n; i++) {
+        int num;
+        cin >> num;
+        og_array.push_back(num);
+    }
+    for (int i = 0; i < m; i++) {
+        vector<int> operation(3);
+        cin >> operation[0] >> operation[1] >> operation[2];
+        operations[i] = operation;
+    }
+    for (int i = 0; i < k; i++) {
+        int x, y;
+        cin >> x >> y;
+        operations_prefix_sum[y - 1] += 1;
+        if (x != 1) operations_prefix_sum[x - 2] -= 1;
+    }
+
+    long long sum = 0;
+    for (int i = 1; i <= m; i++) {
+        sum += operations_prefix_sum[m - i];
+        vector<int> operation = operations[m - i];
+        int l = operation[0];
+        int r = operation[1];
+        int d = operation[2];
+        array_prefix_sum[r - 1] += d * sum;
+        if (l != 1) array_prefix_sum[l - 2] -= d * sum;
+    }
+
+    long long sum_two = 0;
+    for (int i = 1; i <= n; i++) {
+        sum_two += array_prefix_sum[n - i];
+        og_array[n - i] += sum_two;
+    }
+
+    for (long long num : og_array) {
+        cout << num << " ";
+    }
+}
