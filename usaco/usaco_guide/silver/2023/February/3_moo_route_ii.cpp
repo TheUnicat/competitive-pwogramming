473ea545ae7f5802379019da3d902b210e1e85a4
TheUnicat <matthew4680@icloud.com>
2024-08-29 18:11:52 -0700

    update solution

--- usaco/usaco_guide/silver/2023/February/3_moo_route_ii.cpp
+++ usaco/usaco_guide/silver/2023/February/3_moo_route_ii.cpp
@@ -4,23 +4,17 @@
 
 #include <iostream>
 #include <vector>
-#include <queue>
+#include <algorithm>
 using namespace std;
 
-class Comp {
-    public:
-        bool operator()(const pair<int, pair<int, int> > &a, const pair<int, pair<int, int> > &b) {
-            return a.second.first < b.second.first;
-        }
-};
+bool cmp (const pair<int, pair<int, int> > &a, const pair<int, pair<int, int> > &b) {
+    return a.second.first < b.second.first;
+}
 
 int n, m;
-vector<int> layover_times(200000);
 vector<int> min_times(200000, 2000000000);
 
-vector<priority_queue<pair<int, pair<int, int> >, vector<pair<int, pair<int, int> > >,
-Comp > > connections(200000, priority_queue<pair<int, pair<int, int> >, vector<pair<int, pair<int, int> > >,
-Comp >());
+vector<vector<pair<int, pair<int, int> > > > connections(200000, vector<pair<int, pair<int, int> > >());
 
 void dfs(int pos, int time) {
     min_times[pos] = min(min_times[pos], time);
@@ -28,10 +22,10 @@ void dfs(int pos, int time) {
     while (true) {
         if (connections[pos].empty()) break;
 
-        pair<int, pair<int, int> > connection = connections[pos].top();
-        if (connection.second.first < time + layover_times[pos]) break;
+        pair<int, pair<int, int> > connection = connections[pos][connections[pos].size() - 1];
+        if (connection.second.first < time && pos != 0) break;
 
-        connections[pos].pop();
+        connections[pos].pop_back();
         if (connection.second.second >= min_times[connection.first]) continue;
         dfs(connection.first, connection.second.second);
     }
@@ -43,11 +37,16 @@ int main() {
         int c, d, r, s;
         cin >> c >> r >> d >> s;
 
-        connections[c - 1].emplace(make_pair(d - 1, make_pair(r, s)));
+        connections[c - 1].push_back(make_pair(d - 1, make_pair(r, s)));
     }
-    for (int i = 0; i < n; i++) cin >> layover_times[i];
+    for (int i = 0; i < n; i++) {
+        sort(connections[i].begin(), connections[i].end(), cmp);
+        int layover_time;
+        cin >> layover_time;
+        for (int j = 0; j < connections[i].size(); j++) connections[i][j].second.first -= layover_time;
+    }
 
-    dfs(0, -1 * layover_times[0]);
+    dfs(0, 0);
 
     for (int i = 0; i < n; i++) {
         if (min_times[i] == 2000000000) cout << -1 << endl;
