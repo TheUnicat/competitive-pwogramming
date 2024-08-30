3d2dd989b84000652028b532fe365c03708f6da9
TheUnicat <matthew4680@icloud.com>
2024-08-30 13:58:01 -0700

    codeforces contest 969 div2 problem B

--- codeforces/contest_969_2/B_index_and_maximum_value.cpp
+++ codeforces/contest_969_2/B_index_and_maximum_value.cpp
@@ -1,3 +1,36 @@
 //
-// Created by Hong Yang on 2024-08-30.
+// Created by TheUnicat on 2024-08-30.
 //
+
+#include <iostream>
+using namespace std;
+
+int t;
+
+int main() {
+    cin >> t;
+
+    for (int i = 0; i < t; i++) {
+        int n, m;
+        cin >> n >> m;
+        int num, max_num = 0;
+        for (int j = 0; j < n; j++) {
+            cin >> num;
+            max_num = max(max_num, num);
+        }
+
+        for (int j = 1; j <= m; j++) {
+            char sign;
+            int l, r;
+            cin >> sign >> l >> r;
+
+            if (sign == '+') {
+                if (max_num >= l && max_num <= r) max_num++;
+            }
+            else if (max_num >= l && max_num <= r) max_num--;
+
+            cout << max_num << " ";
+        }
+        cout << endl;
+    }
+}
