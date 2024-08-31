9303f6b117e275ec686c699f59c13dfa0b88fb19
TheUnicat <matthew4680@icloud.com>
2024-08-31 01:17:55 -0700

    cleanup

--- usaco/usaco_guide/atcoder/subset_sum_with_add_and_erase.cpp
+++ usaco/usaco_guide/atcoder/subset_sum_with_add_and_erase.cpp
@@ -1,3 +1,37 @@
 //
-// Created by Hong Yang on 2024-08-25.
+// Created by TheUnicat on 2024-08-25.
 //
+
+#include <iostream>
+#include <vector>
+using namespace std;
+using ll = long long;
+
+int mod_n = 998244353;
+vector<ll> combinations(5001, 0);
+
+char sign;
+int ball_val;
+int q, k;
+
+int main() {
+    cin >> q >> k;
+    combinations[0] = 1;
+    for (int i = 0; i < q; i++) {
+        cin >> sign >> ball_val;
+        if (sign == '+') {
+            for (int j = k; j >= ball_val; j--) {
+                combinations[j] += combinations[j - ball_val];
+                combinations[j] %= mod_n;
+            }
+        }
+        else {
+            for (int j = 0; j <= k - ball_val; j++) {
+                combinations[j + ball_val] += mod_n - combinations[j];
+                combinations[j + ball_val] %= mod_n;
+            }
+        }
+
+        cout << combinations[k] << endl;
+    }
+}
--- usaco/usaco_guide/gold/2019/January/1_cow_poetry.cpp
+++ usaco/usaco_guide/gold/2019/January/1_cow_poetry.cpp
@@ -1,3 +1,22 @@
 //
-// Created by Hong Yang on 2024-08-26.
+// Created by TheUnicat on 2024-08-26.
 //
+
+#include <fstream>
+#include <vector>
+using namespace std;
+
+int main() {
+
+}
+
+// 3 3 10
+// 3 1
+// 4 1
+// 3 2
+// A
+// B
+// A
+
+// 2(2) * 2(2) * 2(2)
+// 2(2) *
