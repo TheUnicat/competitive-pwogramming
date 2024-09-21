acb918176a837189f660b59cf501ca4e7fdabe0a
TheUnicat <matthew4680@icloud.com>
2024-09-20 20:00:05 -0700

    cleanup baltic oi 2010 p3 solution

--- baltic_oi/2010/3_printed_circuit_board.cpp
+++ baltic_oi/2010/3_printed_circuit_board.cpp
@@ -2,52 +2,38 @@
 // Created by TheUnicat on 2024-09-14.
 //
 
-#include <fstream>
 #include <algorithm>
 #include <vector>
 #include <iostream>
 using namespace std;
 
 int n;
-int layers = 0;
-vector<pair<int, int>> connectors_temp;
-vector<int> connectors;
-vector<bool> done;
-
-bool cmp(pair<int, int> a, pair<int, int> b) {
-    if (a.first == b.first) {
-        return a.second > b.second;
-    }
-    return a.first < b.first;
-}
+vector<pair<int, int>> connectors;
 
 int main() {
     cin >> n;
     for (int i = 0; i < n; i++) {
         int l, r;
         cin >> l >> r;
-        connectors_temp.emplace_back(l, r);
-        done.push_back(false);
+        connectors.emplace_back(l, r);
     }
-    sort(connectors_temp.begin(), connectors_temp.end(), cmp);
-    for (auto thing : connectors_temp) connectors.push_back(thing.second);
-
+    sort(connectors.begin(), connectors.end());
     vector<int> piles;
-    for (int connection : connectors) {
+    for (auto connection : connectors) {
         int left = 0;
         int right = (int) piles.size() - 1;
         int min_index_less = INT32_MAX;
         while (right >= left) {
             int mid = left + (right - left) / 2;
-            if (piles[mid] < connection) {
+            if (piles[mid] < connection.second) {
                 min_index_less = min(min_index_less, mid);
                 right = mid - 1;
             }
             else left = mid + 1;
         }
 
-        if (min_index_less != INT32_MAX) piles[min_index_less] = connection;
-        else piles.push_back(connection);
+        if (min_index_less != INT32_MAX) piles[min_index_less] = connection.second;
+        else piles.push_back(connection.second);
     }
 
     cout << piles.size();
