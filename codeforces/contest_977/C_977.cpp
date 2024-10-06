2ea71eb13f6186f80f913358403ad177984be855
TheUnicat <matthew4680@icloud.com>
2024-10-06 12:50:14 -0700

    fix codeforces 977 c1

--- codeforces/contest_977/C_977.cpp
+++ codeforces/contest_977/C_977.cpp
@@ -4,6 +4,7 @@
 
 #include <iostream>
 #include <vector>
+#include <set>
 using namespace std;
 
 int t;
@@ -15,24 +16,25 @@ int main() {
         bool possible = true;
         cin >> n >> m >> q;
         vector<int> init_ord(n);
-        vector<int> first_found(n, -1);
+        vector<int> nums(m);
         for (int j = 0; j < n; j++) cin >> init_ord[j];
-        for (int j = 0; j < m; j++) {
-            int num;
-            cin >> num;
-            num--;
-            if (first_found[num] == -1) first_found[num] = j;
-        }
 
-        int prev_pos = -1;
-        for (int j = 0; j < n; j++) {
-            if ((first_found[init_ord[j] - 1] < prev_pos) && (first_found[init_ord[j] - 1] != -1)) {
+        set<int> seen_nums;
+        int curr_index = 0;
+        for (int j = 0; j < m; j++) cin >> nums[j];
+        for (int j = 0; j < m; j++) {
+            int num = nums[j];
+            if (seen_nums.find(num) == seen_nums.end()) {
+                if (curr_index < n && num == init_ord[curr_index]) {
+                    seen_nums.insert(init_ord[curr_index]);
+                    curr_index++;
+                    continue;
+                }
                 possible = false;
                 break;
             }
-            prev_pos = first_found[init_ord[j]];
-        }
 
+        }
 
         if (possible) cout << "YA" << endl;
         else cout << "TIDAK" << endl;
