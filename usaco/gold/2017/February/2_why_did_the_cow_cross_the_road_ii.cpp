eb0322a25efaf34c3a3703847792047690df2951
TheUnicat <matthew4680@icloud.com>
2024-11-27 23:57:10 -0800

    usaco guide 2017 why did the cow the road partial

---
+++ usaco/gold/2017/February/2_why_did_the_cow_cross_the_road_ii.cpp
@@ -0,0 +1,50 @@
+//
+// Created by TheUnicat on 2024-11-27.
+//
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <set>
+#include <algorithm>
+using namespace std;
+
+int n;
+vector<pair<int, int>> left_line, right_line;
+set<pair<int, int>> edges;
+
+int main() {
+    freopen("nocross.in", "r", stdin);
+    freopen("nocross.out", "w", stdout);
+    cin >> n;
+    left_line.resize(n);
+    right_line.resize(n);
+    for (int i = 0; i < n; i++) cin >> left_line[i].first;
+    for (int i = 0; i < n; i++) cin >> right_line[i].first;
+    for (int i = 0; i < n; i++) {
+        left_line[i].second = i;
+        right_line[i].second = i;
+    }
+    sort(left_line.begin(), left_line.end());
+    sort(right_line.begin(), right_line.end());
+
+    for (int i = 0; i < n; i++) {
+        for (int j = i - 4; j <= i + 4; j++) {
+            if (j < 0 || j >= n) continue;
+            edges.emplace(left_line[i].second, right_line[j].second);
+        }
+    }
+
+    vector<vector<int>> dp(n + 1, vector<int>(n + 1, 0));
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n; j++) {
+            dp[i][j] = max(dp[i - 1][j], dp[i][j]);
+            dp[i][j] = max(dp[i][j - 1], dp[i][j]);
+            if (edges.find(make_pair(i, j)) != edges.end()) {
+                dp[i][j] = max(dp[i - 1][j - 1] + 1, dp[i][j]);
+            }
+        }
+    }
+
+    cout << dp[n][n] + 1 << endl;
+}
