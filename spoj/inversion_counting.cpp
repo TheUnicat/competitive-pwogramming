0bb35c5f46b0dffaa635b19d6815e59a6bc2a4fd
TheUnicat <matthew4680@icloud.com>
2024-12-07 15:11:25 -0800

    update solution to use order statistic tree

--- spoj/inversion_counting.cpp
+++ spoj/inversion_counting.cpp
@@ -3,57 +3,29 @@
 //
 
 #include <iostream>
-#include <vector>
+#include <ext/pb_ds/assoc_container.hpp>
 using namespace std;
-using ll = long long;
+using namespace __gnu_pbds;
+using ll = unsigned long long;
+
+template <class T>
+using Tree =
+    tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
 
 int t, n;
-vector<int> nums;
-vector<int> segtree;
-
-int query(int l, int r) {
-    int sum = 0;
-    l += 1e7, r += 1e7;
-    while (l <= r) {
-        if (l % 2 == 1) {
-            sum += segtree[l];
-            l++;
-        }
-        if (r % 2 == 0) {
-            sum += segtree[r];
-            r--;
-        }
-        l /= 2, r /= 2;
-    }
-    return sum;
-}
-
-void visit(int pos) {
-    pos += 1e7;
-    segtree[pos] = 1;
-    pos /= 2;
-    while (pos > 0) {
-        segtree[pos] = segtree[2 * pos] + segtree[2 * pos + 1];
-        pos /= 2;
-    }
-}
 
 int main() {
     cin >> t;
     for (int i = 0; i < t; i++) {
         ll n_inversions = 0;
-        nums.clear();
-        segtree.clear();
         cin >> n;
-        nums.resize(n);
-        segtree.resize(2 * 1e7, 0);
-        for (int j = 0; j < n; j++) cin >> nums[j];
-
+        Tree<int> os_tree;
         for (int j = 0; j < n; j++) {
-            n_inversions += query(nums[j] - 1, 1e7);
-            visit(nums[j] - 1);
+            int num;
+            cin >> num;
+            n_inversions += j - os_tree.order_of_key(num);
+            os_tree.insert(num);
         }
-
         cout << n_inversions << endl;
     }
 }
