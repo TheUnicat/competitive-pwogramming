a8427709ef08edf2c4151a2984f3d77d9c4b7a8d
TheUnicat <matthew4680@icloud.com>
2025-01-15 22:39:33 -0800

    static rmq w/sparse table

--- yosupo/static_rmq.cpp
+++ yosupo/static_rmq.cpp
@@ -7,34 +7,39 @@
 using namespace std;
 
 int n, q;
-vector<int> rmq;
+vector<vector<int>> rmq;
 
-int query(int l, int r) {
-    l += n, r += n;
-    int ans = rmq[l];
-    while (l <= r) {
-        if (l & 1) {
-            ans = min(ans, rmq[l]);
-            l++;
-        }
-        if (r % 2 == 0) {
-            ans = min(ans, rmq[r]);
-            r--;
-        }
-        l /= 2, r /= 2;
+int floor_log(int num) {
+    int ans = 0;
+    while (num >= 2) {
+        num /= 2;
+        ans++;
     }
     return ans;
 }
 
+int query(int l, int r) {
+    int power = floor_log(r - l);
+    return min(rmq[power][l], rmq[power][r - (1 << power)]);
+}
+
 int main() {
     cin >> n >> q;
-    rmq.resize(2 * n);
-    for (int i = 0; i < n; i++) cin >> rmq[n + i];
-    for (int i = n - 1; i >= 0; i--) rmq[i] = min(rmq[i * 2], rmq[i * 2 + 1]);
-    cout << endl;
+    rmq.resize(floor_log(n) + 1);
+    for (int i = 0; i < n; i++) {
+        int num;
+        cin >> num;
+        rmq[0].push_back(num);
+    }
+
+    for (int i = 1; i <= floor_log(n); i++) {
+        for (int j = 0; j <= n - (1 << i); j++) {
+            rmq[i].push_back(min(rmq[i - 1][j], rmq[i - 1][j + (1 << (i - 1))]));
+        }
+    }
     for (int i = 0; i < q; i++) {
         int l, r;
         cin >> l >> r;
-        cout << query(l, r - 1) << endl;
+        cout << query(l, r) << endl;
     }
 }
